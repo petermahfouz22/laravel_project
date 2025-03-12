0290eec190bbd36fb358d2eaf4a235343d9842d1
mhamedelshemy <58799572+mhamedelshemy@users.noreply.github.com>
2025-03-13 00:59:56 +0200

    establishing the project and make migrations

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
 
